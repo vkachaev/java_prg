ce285f178a1379eaabf57b340a1542c55efb6642
vkachaev <kachaevvladimir@gmail.com>
2016-09-28 02:42:28 +0300

    Преобразовали функцию в метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,24 +8,21 @@ public static void main(String[] args)
 		hello("user");
 		hello("Vladimir");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
 
-		double a = 4;
-		double b = 6;
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+		Rectangle r = new Rectangle(4,6);
+
+
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double len) {
-		return len * len;
-	}
 
-	public static double area(double a, double b){
-		return a * b;
-	}
+
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Katya on 28.09.2016.
+ */
+public class Square {
+
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
