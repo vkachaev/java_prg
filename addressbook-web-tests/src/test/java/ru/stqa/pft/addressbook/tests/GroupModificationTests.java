a6fcc4a48a6959c6915ead1e2e09bc4174f56260
vkachaev <kachaevvladimir@gmail.com>
2016-10-09 02:08:10 +0300

    Lecture2#6 Create GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Katya on 09.10.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1update", "test2update", "test3update"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
