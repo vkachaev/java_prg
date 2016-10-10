f8ce66f804a93de6b1f919796b69c9322337acc0
vkachaev <kachaevvladimir@gmail.com>
2016-10-11 02:23:56 +0300

    Lecture1#2 Исправление
    Реализовать то же самое (вычисление расстояния между двумя точками) при
    помощи метода в классе Point, и добавить в созданный в предыдущем пункте
    запускаемый класс примеры использования метода вместо ранее созданной
    функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -16,17 +16,16 @@ public static void main(String[] args) {
 
   System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-  Point p1 = new Point(1, 1);
-  Point p2 = new Point(5, 4);
-  System.out.println("Расстояние между точкой с координатой x1=" + p1.x + "; y1=" + p1.y + " и координатой x2=" + p2.x + "; y2=" + p2.y + " равно " + distance(p1,p2));
+
+  Point p1 = new Point(0, 0);
+  Point p2 = new Point(4, 3);
+  System.out.println("Расстояние между точкой с координатой x1=" + p1.x + "; y1=" + p1.y + " и координатой x2=" + p2.x + "; y2=" + p2.y + " равно " + p1.distance(p2));
 }
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p2.y - p1.y), 2));
+
   }
 
 
-}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,14 +6,27 @@ package ru.stqa.pft.sandbox;
 public class Point {
   double x;
   double y;
+  private double у;
 
 
   public Point(double x, double y){
     this.x = x;
     this.y = y;
 
+
   }
+  double distance(double х, double у) {
+
+    double dx = this.x - х;
+    double dy = this.у - у;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+  public double distance(Point p2) {
+
+    return distance(p2.x, p2.y);
 
 
+  }
 
 }
