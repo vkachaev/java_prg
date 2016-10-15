0227527b7167f230e4825e4718bd6f6f55aff192
vkachaev <kachaevvladimir@gmail.com>
2016-10-15 21:01:13 +0300

    Lecture3#2 Added Equation class and test methods

---
+++ sandbox/src/test/java/ru/stqa/pft/EquationTests.java
@@ -0,0 +1,48 @@
+package ru.stqa.pft;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Equation;
+
+/**
+ * Created by Katya on 15.10.2016.
+ */
+public class EquationTests {
+  @Test
+  public void test0(){
+    Equation e = new Equation(1,1,1);
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void test1(){
+    Equation e = new Equation(1,2,1);
+    Assert.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void test2(){
+    Equation e = new Equation(1,5,1);
+    Assert.assertEquals(e.rootNumber(),2);
+
+  }
+
+  @Test
+  public void testLinear(){
+    Equation e = new Equation(0,1,1);
+    Assert.assertEquals(e.rootNumber(),1);
+    }
+
+  @Test
+  public void testConstant(){
+    Equation e = new Equation(0,0,1);
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void testZero(){
+    Equation e = new Equation(0,0,0);
+    Assert.assertEquals(e.rootNumber(),-1);
+  }
+  }
+
