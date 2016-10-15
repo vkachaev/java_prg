b2b485e63e50974a5722b983457d25fa6ec4433a
vkachaev <kachaevvladimir@gmail.com>
2016-10-15 22:36:56 +0300

    Lecture3#8 Code optimization - default values and lazy fields filling
    Реализовать продемонстрированые в лекциях оптимизации:
    
    Возможность оставлять дефолтные значения для полей
    Не менять значение в поле, если новое значение совпадает со старым
    Проверять перед каждым переходом, действительно ли этот переход нужно
    делать

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1update", "test2update", "test3update"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -12,7 +12,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
